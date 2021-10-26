6f591350bdada9f6a6a1a5f297e7b9baf688c9d9
emontilla07 <emontilla107@gmail.com>
2021-10-26 16:19:10 -0500

    Cart Value

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,34 @@ let deck       = [];
 const types    = ['C', 'D', 'H', 'S',];
 const specials = ['A', 'J', 'Q', 'K',];
 
+// Está función crea y revuelve las cartas
 const createDeck = () => {
     for ( let i = 2; i <= 10; i++ ) {
         for ( let ty of types ) {
-            deck.push( `${i}${ty}` );
+            deck.push(`${i}${ty}`);
         }
     }
 
-    for ( let ty of types ) {
-        for ( let special of specials ) {
-            deck.push( `${special}${ty}` );
+    for (let ty of types) {
+        for (let special of specials) {
+            deck.push(`${special}${ty}`);
         }
     }
-    console.log( deck );
-    deck = _.shuffle( deck );
-    console.log( deck );
+    // console.log( deck );
+    deck = _.shuffle(deck);
+    console.log(deck);
     return deck;
 }
 
 createDeck();
+
+// Está función permite a los jugadores tomar una carta
+const takeACart = () => {
+    if (deck.length === 0) throw 'No hay cartas en el deck';
+    const cart = deck.pop();
+    console.log(deck);
+    console.log(cart);
+    return cart;
+}
+
+takeACart();
